a7f8e014b750f27d465952ff87392fe6b7bffe82
Jinshi Zhang <zhangjinshi_123@126.com>
2016-12-22 23:24:20 +0800

    the first version of a tcpserver

---
+++ str_echo.c
@@ -0,0 +1,17 @@
+#include	"lib/unp.h"
+
+void
+str_echo(int sockfd)
+{
+	ssize_t		n;
+	char		buf[MAXLINE];
+
+again:
+	while ( (n = read(sockfd, buf, MAXLINE)) > 0)
+		write(sockfd, buf, n);
+
+	if (n < 0 && errno == EINTR)
+		goto again;
+	else if (n < 0)
+		printf("str_echo: read error");
+}
